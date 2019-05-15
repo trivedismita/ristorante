6820f4600acdc2775e51164a2b05b3981a42c279
trivedismita <trivedismita@yahoo.com>
2019-05-15 16:03:36 -0700

    ristorante react app

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,124 @@
+import React, {Component} from 'react';
+import { Navbar , NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron, 
+    Modal, Button, ModalHeader, ModalBody,ModalFooter, Form, FormGroup, Input, Label} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
+        };
+
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        })
+    }
+
+    handleLogin(event){
+        this.toggleModal();
+        alert("Username: "+ this.username.value + "Password: "+ this.password.value + "Remember: "+ this.remember.checked);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <>
+               <Navbar dark expand="md">
+                    <div className="container">
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="assets/images/logo.png" height="30" height="41" alt="Yummy Pizza" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg"></span>Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg"></span>About us
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg"></span>Menu
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg"></span>Contact us
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign-in fa-lg"></span>Login
+                                    </Button>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+                <Jumbotron>
+                    <div className="container">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
+                                <h1>Ristorante </h1>
+                                <p>Sit labore dolore sit irure velit occaecat et quis est. Do nisi ea adipisicing ea irure anim pariatur nulla. Veniam proident magna do esse magna ad. Eu aliquip irure elit tempor reprehenderit aliquip tempor commodo aliqua deserunt exercitation do fugiat. Quis laboris ad velit nulla et nisi id minim sunt exercitation id elit do. Voluptate voluptate qui occaecat occaecat veniam qui enim exercitation incididunt.</p>
+                            </div>  
+                        </div>
+                    </div>
+                </Jumbotron>
+
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember" />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button type="submit" value="submit" className="primary">Login</Button>
+                        <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+            </>
+        );
+    }
+}
+
+
+export default Header;
---
+++ src/shared/dishes.js
@@ -0,0 +1,63 @@
+export const DISHES =
+    [
+        {
+        id: 0,
+        name:'Uthappizza',
+        image: '/assets/images/pizza.jpeg',
+        category: 'mains',
+        label:'Hot',
+        price:'4.99',
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
+        },
+        {
+        id: 1,
+        name:'Zucchipakoda',
+        image: '/assets/images/blur-bowl.jpg',
+        category: 'appetizer',
+        label:'',
+        price:'1.99',
+        featured: false,
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+        },
+        {
+        id: 2,
+        name:'Burrito',
+        image: '/assets/images/burrito.jpeg',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        featured: false,
+        description:'A quintessential ristorante experience, is it a vada or is it a donut?'
+        },
+        {
+        id: 3,
+        name:'Pan Cake',
+        image: '/assets/images/pancake.jpeg',
+        category: 'dessert',
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        },
+        {
+        id: 4,
+        name:'Salad',
+        image: '/assets/images/salad.jpeg',
+        category: 'mains',
+        label:'',
+        price:'4.99',
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
+        },
+        {
+        id: 5,
+        name:'Soup',
+        image: '/assets/images/soup.jpeg',
+        category: 'mains',
+        label:'Hot',
+        price:'4.99',
+        featured: true,
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                    
+        }
+    ];
